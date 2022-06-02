98d93d7ae73d52d158fc2d0c23bec7a9f69e368c
SharonMutabonwa <mutaron37@gmail.com>
2022-06-02 18:25:04 +0000

    A program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
-        print("{} argument:".format(argc -1))
+        print("{} argument:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
 
-    for i in range(0, argc):
+    for i in range(1, argc):
         print("{}: {}".format(i, sys.argv[i]))
