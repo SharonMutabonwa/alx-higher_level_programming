30d526c4f141f70909a83e11d1d38ade50a4af4a
SharonMutabonwa <mutaron37@gmail.com>
2022-06-02 18:58:53 +0000

    A program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pytthon3
+if __name__ = "__main__"
+    from hidden_4 import *
+    nam == dir()
+    for i in range(0, len(nam)):
+        if nam[i][0:2] != "__":
+            print("{}".format(nam[i]))
