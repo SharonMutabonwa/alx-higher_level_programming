90077ce7201005b819f3e076500af85b8160c7c3
SharonMutabonwa <mutaron37@gmail.com>
2022-06-20 20:18:46 +0000

    A function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
     except NotRealNumber:
         print("The number is not real ")
     else:
-        print("{}.format(x)")
+        print("{:d}.format(x)")
     finally:
         print(" ")
