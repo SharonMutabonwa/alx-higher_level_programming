73ad174aae3bee9cc44ed55a367868dce2f5c83c
SharonMutabonwa <mutaron37@gmail.com>
2022-07-11 23:34:25 +0000

    Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,6 +10,7 @@ class TestRectangle(unittest.TestCase):
     """Define unit test for Rectangle model"""
 
     def test_initialization(self):
+        '''Test Rectangle class'''
         r1 = Rectangle(2, 5)
         self.assertEqual(r1.id, Rectangle._Base__nb_objects)
         r2 = Rectangle(1, 2)
