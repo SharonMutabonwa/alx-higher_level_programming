b11e2e1aa71b97b9a6a6f2149a39c42b1d5dc7d1
SharonMutabonwa <mutaron37@gmail.com>
2022-09-24 00:29:28 +0200

    fixed error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+from urllib.request import urlopen, Request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        data = response.read()
+
+        # decode gets the string representation of the message
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf8")))
