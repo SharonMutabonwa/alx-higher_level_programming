c307052e1a656e221b605cbde014c86d165dd7d9
SharonMutabonwa <mutaron37@gmail.com>
2022-09-23 19:06:36 +0200

    fixed error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
-
+from urllib import request
 
 if __name__ == "__main__":
-    from urllib import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        html = resp.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
